436cd0b5e9082ff6b53a6e60d147fb6ac7e54350
jpbezerra <jubalima2005@gmail.com>
2024-08-14 02:47:04 -0300

    Create A - Helpful Maths.java

---
+++ Rounds/Codeforces Round 197 (Div. 2)/A - Helpful Maths.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String equation = scanner.next();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int size = equation.length();
+        for (int i = 0; i < size; i++) {
+            if (equation.charAt(i) != '+') {
+                arr.add(equation.charAt(i) - '0');
+            }
+        }
+
+        Collections.sort(arr);
+        System.out.printf("%d", arr.get(0));
+        size = arr.size();
+        for (int i = 1; i < size; i++) {
+            System.out.printf("+%d", arr.get(i));
+        }
+    }
+}
