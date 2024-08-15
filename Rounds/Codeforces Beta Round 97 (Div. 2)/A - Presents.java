a3c345cbb2e59a6e7389e21734f754519e084300
jpbezerra <jubalima2005@gmail.com>
2024-08-15 10:43:00 -0300

    Update A - Presents.java

--- Rounds/Codeforces Beta Round 97 (Div. 2)/A - Presents.java
+++ Rounds/Codeforces Beta Round 97 (Div. 2)/A - Presents.java
@@ -1,22 +1,17 @@
-import java.util.ArrayList;
 import java.util.Scanner;
- 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int cases = scanner.nextInt();
-        ArrayList<Integer> result = new ArrayList<>();
- 
+        int[] result = new int[cases];
+
         for (int i = 0; i < cases; i++) {
-            result.add(0);
+            result[scanner.nextInt() - 1] = i + 1;
         }
- 
+
         for (int i = 0; i < cases; i++) {
-            result.set(scanner.nextInt() - 1, i + 1);
-        }
- 
-        for (int i = 0; i < cases; i++) {
-            System.out.printf("%d ", result.get(i));
+            System.out.printf("%d ", result[i]);
         }
     }
 }
