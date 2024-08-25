3e96044b61ce504f76c74748b735018ef59a88ac
jpbezerra <jubalima2005@gmail.com>
2024-08-25 19:46:47 -0300

    Create A - Team Olympiad.cpp

---
+++ codeforces/Codeforces Round 279 (Div. 2)/A - Team Olympiad.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   int cases;
+   cin >> cases;
+
+   vector<int> first, second, third;
+   first.reserve(cases);
+   second.reserve(cases);
+   third.reserve(cases);
+
+   for (int i = 0; i < cases; i++) {
+      int temp;
+      cin >> temp;
+
+      if (temp == 1) {
+         first.push_back(i + 1);
+      }
+
+      else if (temp == 2) {
+         second.push_back(i + 1);
+      }
+
+      else {
+         third.push_back(i + 1);
+      }
+   }
+
+   int size = min(first.size(), min(second.size(), third.size()));
+   cout << size << '\n';
+
+   for (int i = 0; i < size; i++) {
+      cout << first[i] << ' ' << second[i] << ' ' << third[i] << '\n';
+   }
+
+   return 0;
+}
